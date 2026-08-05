90e11d387c8f46c63a47a7b563c1514f3904e6b1
Gabskit <gaby.mofa31@gmail.com>
2026-08-05 14:53:47 -0600

    Multiplicación y división

--- Src/Vm/Src/Calc.c
+++ Src/Vm/Src/Calc.c
@@ -97,6 +97,9 @@ PaxoNum8 add_num8(PaxoNum8 a, PaxoNum8 b) {
     abs_suma /= 10;
     exp++;}
 
+	if (exp < 0) exp = 0;
+	if (exp > 1) exp = 1;
+
 	result.exponente = (unsigned _BitInt(1))exp + sesgo;
 	result.entero = (unsigned _BitInt(4))(abs_suma >> 2) & 0xf;
 	result.fraccion = (unsigned _BitInt(2))abs_suma & 0x3;
@@ -141,10 +144,65 @@ PaxoNum8 sub_num8(PaxoNum8 a, PaxoNum8 b) {
     abs_suma /= 10;
     exp++;}
 
+	if (exp < 0) exp = 0;
+	if (exp > 1) exp = 1;
+
 	result.exponente = (unsigned _BitInt(1))exp + sesgo;
 	result.entero = (unsigned _BitInt(4))(abs_suma >> 2) & 0xf;
 	result.fraccion = (unsigned _BitInt(2))abs_suma & 0x3;
 	return result;}
+
+PaxoNum8 mul_num8(PaxoNum8 a, PaxoNum8 b) {
+	const uint8_t sesgo = 0;
+	int32_t val_a = a.fraccion + (a.entero * 4);
+  int32_t val_b = b.fraccion + (b.entero * 4);
+
+	uint8_t signo_final = (a.signo != b.signo) ? 1 : 0;
+	int32_t mult = (val_a * val_b) >> 2;
+	int16_t exp_final = (int16_t)a.exponente + (int16_t)b.exponente - sesgo;
+
+	while (mult > (1 << 6)) {
+    mult /= 10;
+    exp_final++;}
+
+	if (exp_final < 0) exp_final = 0;
+  if (exp_final > 1) exp_final = 1;
+
+	PaxoNum16 result;
+  result.signo = signo_final;
+  result.exponente = (unsigned _BitInt(1))exp_final;
+  result.entero = (unsigned _BitInt(4))(mult >> 2) & 0xf;
+	result.fraccion = (unsigned _BitInt(2))mult & 0x3;
+	return result;}
+
+PaxoNum8 div_num8(PaxoNum8 a, PaxoNum8 b) {
+	const uint8_t sesgo = 0;
+	int32_t val_a = a.fraccion + (a.entero * 4);
+  int32_t val_b = b.fraccion + (b.entero * 4);
+
+	if (val_b == 0) {
+    PaxoNum8 error = {0, 0, 0, 0};
+    return error;
+	}
+
+	uint8_t signo_final = (a.signo != b.signo) ? 1 : 0;
+	int32_t mult = (val_a << 2) / val_b; 
+
+	int16_t exp_final = (int16_t)a.exponente - (int16_t)b.exponente + sesgo;
+
+	while (mult > (1 << 6)) {
+    mult /= 10;
+    exp_final++;}
+
+	if (exp_final < 0) exp_final = 0;
+  if (exp_final > 1) exp_final = 1;
+
+  PaxoNum16 result;
+  result.signo = signo_final;
+  result.exponente = (unsigned _BitInt(1))exp_final;
+  result.entero = (unsigned _BitInt(4))(mult >> 6) & 0xf;
+  result.fraccion = (unsigned _BitInt(2))mult & 0x3;
+  return result;}
 
 //16 bits
 PaxoNum16 add_num16(PaxoNum16 a, PaxoNum16 b) {
@@ -185,6 +243,9 @@ PaxoNum16 add_num16(PaxoNum16 a, PaxoNum16 b) {
 	while (abs_suma > (1 << 11)) {
     abs_suma /= 10;
     exp++;}
+
+	if (exp < 0) exp = 0;
+	if (exp > 15) exp = 15;
 
 	result.exponente = (unsigned _BitInt(4))exp + sesgo;
 	result.entero = (unsigned _BitInt(5))(abs_suma >> 6) & 0x1f;
@@ -230,10 +291,66 @@ PaxoNum16 sub_num16(PaxoNum16 a, PaxoNum16 b) {
     abs_suma /= 10;
     exp++;}
 
+	if (exp < 0) exp = 0;
+	if (exp > 15) exp = 15;
+
+
 	result.exponente = (unsigned _BitInt(4))exp + sesgo;
 	result.entero = (unsigned _BitInt(5))(abs_suma >> 6) & 0x1f;
 	result.fraccion = (unsigned _BitInt(6))abs_suma & 0x3f;
 	return result;}
+
+PaxoNum16 mul_num16(PaxoNum16 a, PaxoNum16 b) {
+	const uint8_t sesgo = 7;
+	int32_t val_a = a.fraccion + (a.entero * 64);
+  int32_t val_b = b.fraccion + (b.entero * 64);
+
+	uint8_t signo_final = (a.signo != b.signo) ? 1 : 0;
+	int32_t mult = (val_a * val_b) >> 6;
+	int16_t exp_final = (int16_t)a.exponente + (int16_t)b.exponente - sesgo;
+
+	while (mult > (1 << 11)) {
+    mult /= 10;
+    exp_final++;}
+
+	if (exp_final < 0) exp_final = 0;
+  if (exp_final > 15) exp_final = 15;
+
+	PaxoNum16 result;
+  result.signo = signo_final;
+  result.exponente = (unsigned _BitInt(4))exp_final;
+  result.entero = (unsigned _BitInt(5))(mult >> 6) & 0x1f;
+	result.fraccion = (unsigned _BitInt(6))mult & 0x3f;
+	return result;}
+
+PaxoNum16 div_num16(PaxoNum16 a, PaxoNum16 b) {
+	const uint8_t sesgo = 7;
+	int32_t val_a = a.fraccion + (a.entero * 64);
+  int32_t val_b = b.fraccion + (b.entero * 64);
+
+	if (val_b == 0) {
+    PaxoNum16 error = {0, 0, 0, 0};
+    return error;
+	}
+
+	uint8_t signo_final = (a.signo != b.signo) ? 1 : 0;
+	int32_t mult = (val_a << 6) / val_b; 
+
+	int16_t exp_final = (int16_t)a.exponente - (int16_t)b.exponente + sesgo;
+
+	while (mult > (1 << 11)) {
+    mult /= 10;
+    exp_final++;}
+
+	if (exp_final < 0) exp_final = 0;
+  if (exp_final > 15) exp_final = 15;
+
+  PaxoNum16 result;
+  result.signo = signo_final;
+  result.exponente = (unsigned _BitInt(4))exp_final;
+  result.entero = (unsigned _BitInt(5))(mult >> 6) & 0x1f;
+  result.fraccion = (unsigned _BitInt(6))mult & 0x3f;
+  return result;}
 
 //32 bits
 PaxoNum32 add_num32(PaxoNum32 a, PaxoNum32 b) {
@@ -274,6 +391,9 @@ PaxoNum32 add_num32(PaxoNum32 a, PaxoNum32 b) {
 	while (abs_suma > (1 << 25)) {
     abs_suma /= 10;
     exp++;}
+
+	if (exp < 0) exp = 0;
+	if (exp > 63) exp = 63;
 
 	result.exponente = (unsigned _BitInt(6))exp + sesgo;
 	result.entero = (unsigned _BitInt(12))(abs_suma >> 13) & 0xfff;
@@ -319,7 +439,62 @@ PaxoNum32 sub_num32(PaxoNum32 a, PaxoNum32 b) {
     abs_suma /= 10;
     exp++;}
 
+	if (exp < 0) exp = 0;
+	if (exp > 63) exp = 63;
+
 	result.exponente = (unsigned _BitInt(6))exp + sesgo;
 	result.entero = (unsigned _BitInt(12))(abs_suma >> 13) & 0xfff;
 	result.fraccion = (unsigned _BitInt(13))abs_suma & 0x1fff;
 	return result;}
+
+PaxoNum32 mul_num32(PaxoNum32 a, PaxoNum32 b) {
+	const uint8_t sesgo = 31;
+	int32_t val_a = a.fraccion + (a.entero * 8192);
+  int32_t val_b = b.fraccion + (b.entero * 8192);
+
+	uint8_t signo_final = (a.signo != b.signo) ? 1 : 0;
+	int32_t mult = (val_a * val_b) >> 13;
+	int16_t exp_final = (int16_t)a.exponente + (int16_t)b.exponente - sesgo;
+
+	while (mult > (1 << 25)) {
+    mult /= 10;
+    exp_final++;}
+
+	if (exp_final < 0) exp_final = 0;
+  if (exp_final > 63) exp_final = 63;
+
+	PaxoNum16 result;
+  result.signo = signo_final;
+  result.exponente = (unsigned _BitInt(6))exp_final;
+  result.entero = (unsigned _BitInt(12))(mult >> 13) & 0xfff;
+	result.fraccion = (unsigned _BitInt(13))mult & 0x1fff;
+	return result;}
+
+PaxoNum32 div_num32(PaxoNum32 a, PaxoNum32 b) {
+	const uint8_t sesgo = 31;
+	int32_t val_a = a.fraccion + (a.entero * 8192);
+  int32_t val_b = b.fraccion + (b.entero * 8192);
+
+	if (val_b == 0) {
+    PaxoNum16 error = {0, 0, 0, 0};
+    return error;
+	}
+
+	uint8_t signo_final = (a.signo != b.signo) ? 1 : 0;
+	int32_t mult = (val_a << 13) / val_b; 
+
+	int16_t exp_final = (int16_t)a.exponente - (int16_t)b.exponente + sesgo;
+
+	while (mult > (1 << 25)) {
+    mult /= 10;
+    exp_final++;}
+
+	if (exp_final < 0) exp_final = 0;
+  if (exp_final > 63) exp_final = 63;
+
+  PaxoNum16 result;
+  result.signo = signo_final;
+  result.exponente = (unsigned _BitInt(6))exp_final;
+  result.entero = (unsigned _BitInt(12))(mult >> 6) & 0xfff;
+  result.fraccion = (unsigned _BitInt(13))mult & 0x1fff;
+  return result;}
